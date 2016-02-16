02f85b57d10e670aa75d3bdaad546ed695bd62f1
Jeff Ciesielski <jciesiel@impinj.com>
2016-02-16 09:54:17 -0500

    Fix cstrmatch
    Also: enable debug symbols and line numbers in nim

--- libs/narm_runtime/cstrmatch.nim
+++ libs/narm_runtime/cstrmatch.nim
@@ -2,5 +2,6 @@
 proc c_strncmp(s1, s2: cstring, n: int32): cint {.cdecl, importc: "strncmp", header: "string.h".}
 
 template `==`* (a: cstring, b: string): bool =
-  (c_strncmp(a, b.cstring, b.len) == 0)
+  # The +1 handles the terminating zero to ensure 'foo' doesn't match 'foobarbaz'
+  (c_strncmp(a, b.cstring, b.len+1) == 0)
   
