895c06b66453ccdf41d9403cea8be9f39bdf96b5
Jeff Ciesielski <jciesiel@impinj.com>
2016-02-18 23:36:56 -0500

    Clean up assertions

--- libs/narm_runtime/assertions.nim
+++ libs/narm_runtime/assertions.nim
@@ -1,30 +1,33 @@
 import stdio
 import strutils
 
-
-template assertFatal*(assertion: bool, message: string): void =
+template assertFatal*(assertion: bool): void =
   if assertion == true:
     const position = instantiationInfo()
     const fnSplit = position.filename.split("/")
     const fileName = fnSplit[fnSplit.len - 1]
-    printf("Assertion failed at - %s: %s\n", fileName, $position.line)
-    printf("Fatal: %s\n", message)
+    printf("Fatal: Assertion failed @ %s: %s\n", fileName, $position.line)
     while true:
       discard
 
-template assertWarn*(assertion: untyped, message: string): void =
+template assertWarn*(assertion: bool): void =
   if assertion != true:
     const position = instantiationInfo()
     const fnSplit = position.filename.split("/")
     const fileName = fnSplit[fnSplit.len - 1]
-    printf("Assertion failed at - %s: %s\n", fileName, $position.line)
-    printf("Warning, failed assertion. %s\n", message)
+    printf("Warning: Assertion failed @ %s: %s\n", fileName, $position.line)
 
 template errFatal*(message: string): void =
-  printf("Fatal error: %s\n", message)
+  const position = instantiationInfo()
+  const fnSplit = position.filename.split("/")
+  const fileName = fnSplit[fnSplit.len - 1]
+  printf("Fatal error: %s @ %s:%s\n", message, fileName, $position.line)
   while true:
     discard
 
 template errWarning*(message: string): void =
-  printf("Warning error: %s\n", message)
+  const position = instantiationInfo()
+  const fnSplit = position.filename.split("/")
+  const fileName = fnSplit[fnSplit.len - 1]
+  printf("Warning: %s @ %s:%s\n", message, fileName, $position.line)
   
