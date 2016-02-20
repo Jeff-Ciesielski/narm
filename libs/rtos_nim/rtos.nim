6d95ea244f147866a6e75ed3e839abf689481892
Jeff Ciesielski <jciesiel@impinj.com>
2016-02-19 23:45:40 -0500

    Add semaphore and notification support

--- libs/rtos_nim/rtos.nim
+++ libs/rtos_nim/rtos.nim
@@ -1,11 +1,18 @@
 import assertions
 
-var MaxDelay* {.importc: "portMAX_DELAY", header: "FreeRTOSWrap.h".}: cint
+var MaxDelay* {.importc: "portMAX_DELAY", header: "FreeRTOSWrap.h".}: uint32
 
 type
   # Task related types
   Task* = (proc(params: pointer):void {.cdecl.})
   TaskHandle* = pointer
+
+  NotificationAction {.pure.} = enum
+    None = 0,
+    SetBits,
+    Increment,
+    SetWithOverwrite,
+    SetWithoutOverwrite,
 
   # Timer related types
   TimerHandle* = pointer
@@ -24,6 +31,15 @@ proc xTaskCreate(
     parameters: pointer,
     priority: uint32,
     created_task: TaskHandle): uint32 {.importc: "xTaskCreate", header: "FreeRTOSWrap.h", cdecl.}
+proc xTaskNotify(
+    t: TaskHandle,
+    value: uint32,
+    action: NotificationAction): bool {.importc: "xTaskNotify", header: "FreeRTOSWrap.h", cdecl.}
+proc xTaskNotifyWait(
+    bitsClearedOnEntry: uint32,
+    bitsClearedOnExit: uint32,
+    notificationAction: ptr uint32,
+    ticksToWait: uint32): bool {.importc: "xTaskNotifyWait", header:"FreeRTOSWrap.h", cdecl.}
 
 proc xTimerCreate(
     timerName: cstring,
@@ -34,7 +50,7 @@ proc xTimerCreate(
 
 proc vSemaphoreCreateBinary(smphr: SemaphoreHandle): void {.importc: "vSemaphoreCreateBinary", header:"FreeRTOSWrap.h", cdecl.}
 proc xSemaphoreGive(smphr: SemaphoreHandle): bool {.importc: "vSemaphoreCreateBinary", header:"FreeRTOSWrap.h", cdecl.}
-proc xSemaphoreTake(smphr: SemaphoreHandle, blockTime: int): bool {.importc: "vSemaphoreCreateBinary", header:"FreeRTOSWrap.h", cdecl.}
+proc xSemaphoreTake(smphr: SemaphoreHandle, blockTime: uint32): bool {.importc: "vSemaphoreCreateBinary", header:"FreeRTOSWrap.h", cdecl.}
 
 proc xTimerStart(timer: TimerHandle, ticksToWayt: uint32): bool {.importc: "xTimerStart", header:"FreeRTOSWrap.h", cdecl.}
 
@@ -48,7 +64,6 @@ template rtosStackOverflowHook*(actions: untyped): void =
 template rtosApplicationTickHook*(actions: untyped): void =
   proc vApplicationTickHook(): void {.exportc: "vApplicationTickHook", cdecl.} =
     actions
-  
 
 # Task management functions
 
@@ -62,8 +77,17 @@ proc createTask*(t: Task, taskName: cstring, stackSize: uint16, parameters: poin
 proc deleteTask*(t: TaskHandle): void =
   vTaskDelete(t)
 
-proc startScheduler*(): void  =
+template startScheduler*(): void  =
   vTaskStartScheduler()
+
+proc notify*(t: TaskHandle, value: uint32, action: NotificationAction): bool =
+  result = xTaskNotify(t, value, action)
+
+proc wait*(bitsClearedOnEntry, bitsClearedOnExit: uint32, ticksToWait: uint32 = MaxDelay): auto =
+  var notifyValue: uint32
+  var retVal = xTaskNotifyWait(bitsClearedOnEntry, bitsClearedOnExit, notifyValue.addr, ticksToWait)
+
+  return (retVal, notifyValue)
 
 # Timer management
 template timerHandler*(name, actions: untyped): void =
@@ -83,5 +107,5 @@ proc createBinarySemaphore*(): SemaphoreHandle =
 proc give*(smphr: SemaphoreHandle): bool =
   result = xSemaphoreGive(smphr)
 
-proc take*(smphr: SemaphoreHandle, blockTime: int = MaxDelay): bool =
+proc take*(smphr: SemaphoreHandle, blockTime: uint32 = MaxDelay): bool =
   result = xSemaphoreTake(smphr, blockTime)
