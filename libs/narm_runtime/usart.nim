ff98beaca2a3e2456daa16bbb83d917514b02ffa
Jeff Ciesielski <jciesiel@impinj.com>
2016-06-30 11:13:44 -0400

    Fix usart function signature
    example now runs!

--- libs/narm_runtime/usart.nim
+++ libs/narm_runtime/usart.nim
@@ -8,7 +8,7 @@ const
 proc usart_init(usart_no: int, baudrate: uint32): void {.importc.}
 
 proc write*(usartNo: int, source: cstring, length: int): int {.importc: "usart_write", header: "usart.h", cdecl.}
-proc read*(usartNo: int, dest: cstring, length: int, timeout: uint32): int {.importc: "usart_read", header: "usart.h", cdecl.}
+proc read*(usartNo: int, dest: cstring, length: int, timeout: int): int {.importc: "usart_read", header: "usart.h", cdecl.}
 
 # Public API
 proc init*(usart_no: int, baudrate: uint32): void =
