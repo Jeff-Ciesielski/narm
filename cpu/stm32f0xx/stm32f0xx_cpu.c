e1ce2bb37a1c22350324a7cd7bfb86693a0cbe7e
Jeff Ciesielski <jciesiel@impinj.com>
2016-06-30 14:26:40 -0400

    Build out systick support in stm32f0xx_cpu

--- cpu/stm32f0xx/stm32f0xx_cpu.c
+++ cpu/stm32f0xx/stm32f0xx_cpu.c
@@ -1,11 +1,21 @@
 #include <cpu.h>
+#include <stdio.h>
+#include "stm32f0xx.h"
+
+volatile static uint64_t ticks = 0;
+
+void SysTick_Handler(void)
+{
+	ticks += 1;
+}
 
 int system_init(void)
 {
+	SysTick_Config(SystemCoreClock / 1000);
 	return 0;
 }
 
 uint64_t get_system_time(void)
 {
-	return 0;
+	return ticks;
 }
