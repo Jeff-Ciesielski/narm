7bb29df2711faa25f847a432b352411bc6a947ce
Jeff Ciesielski <jeffciesielski@gmail.com>
2016-07-01 11:52:24 -0400

    Update assertions to only use raw filename

--- libs/narm_runtime/assertions.nim
+++ libs/narm_runtime/assertions.nim
@@ -1,7 +1,8 @@
 import stdio
 
+
 template assertFatal*(assertion: bool): void =
-  const filename = instantiationInfo().filename
+  const filename = instantiationInfo(-1, false).filename
   const position = instantiationInfo().line
   if assertion == true:
     printf("Fatal: Assertion failed @ %s: %d\n", fileName, position)
@@ -9,21 +10,21 @@ template assertFatal*(assertion: bool): void =
       discard
 
 template assertWarn*(assertion: bool): void =
-  const filename = instantiationInfo().filename
+  const filename = instantiationInfo(-1, false).filename
   const position = instantiationInfo().line
   if assertion != true:
     printf("Warning: Assertion failed @ %s: %d\n", filename, position)
     return
 
 template errFatal*(message: string): void =
-  const filename = instantiationInfo().filename
+  const filename = instantiationInfo(-1, ).filename
   const position = instantiationInfo().line
   printf("Fatal error: %s @ %s:%d\n", message, filename, position)
   while true:
     discard
 
 template errWarning*(message: string): void =
-  const filename = instantiationInfo().filename
+  const filename = instantiationInfo(-1, false).filename
   const position = instantiationInfo().line
   printf("Warning: %s @ %s:%d\n", message, pos.filename, $position.line)
   return
